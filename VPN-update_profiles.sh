21f794ff12ec2262054996ab33731cb826cff434
Filippo Falezza <filippo.falezza@outlook.it>
2020-04-27 13:11:37 +0200

    Add files via upload

---
+++ VPN-update_profiles.sh
@@ -0,0 +1,18 @@
+# Author: Filippo Falezza
+# ¢ 2020 GPLv3
+# 
+# This script is to be executed from the folder where all the profiles to import are.
+#
+# DEFINE your username and password here:
+USERNAME=
+PASSWORD=
+#
+#
+for file in $PWD/*.ovpn
+do
+	name="$(basename $file .ovpn)"
+#	echo $file $name
+	nmcli c del $name
+	nmcli c import type openvpn file $file
+	printf "set vpn.user-name $USERNAME\n set vpn.secrets password=$PASSWORD\n save\n quit\n"| nmcli c edit $name
+done
