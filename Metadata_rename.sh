0920d434722bcd98c4a9207f24e5995a5879a70a
Filippo Falezza <filippo.falezza@outlook.it>
2022-08-07 21:04:44 +0200

    Update Metadata_rename.sh

--- Metadata_rename.sh
+++ Metadata_rename.sh
@@ -3,10 +3,13 @@
 # ©Filippo Falezza 2022, GPLv3 and Later
 # This script wants to rename files based on their metadata. It is useful when used together with photorec, to automatically rename any recovered file
 #
-for file in $(ls .|grep jpg);
+# Please edit accordingly for any timezone that's not UTC+1 or UTC+2
+EXTENSION=mp4
+
+for file in $(ls .|grep $EXTENSION);
 do
 	string=$(exiftool $file|grep -e "File Modification Date/Time"|sed -e 's/^File Modification Date\/Time     : //'|sed -e 's/://'|sed -e 's/://'|sed -e 's/ /_&/'|sed -e 's/ //'|sed -e 's/://'|sed -e 's/://'|sed -e 's/\+0//'|sed -e 's/1|2//'|sed -e 's/:00//')
-	mv $file $string.jpg
-	echo IMG_$string.jpg
+	mv $file $string.$EXTENSION
+	echo IMG_$string.$EXTENSION
 done
 
